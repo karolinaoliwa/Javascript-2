516e85be2d078481f03c0a79055949114f0e35b4
Karolina Oliwa <hethell93@gmail.com>
2018-01-08 20:36:29 +0100

    Add changes.

--- 9.5/scripts.js
+++ 9.5/scripts.js
@@ -1,8 +1,8 @@
 var buttonText = document.getElementsByClassName("button");
- 	x = buttonText.length;
+var textLength = buttonText.length;
 
 
-for ( var i = 0 ; i < x ; i++) {
+for ( var i = 0 ; i < textLength ; i++) {
   var text = buttonText[i].innerText;
   alert("Button Text: " + text);
 }
